3253b985a1302bb3c8bc7d1bcf380e9ee52903da
Gabriel Toschi <gabriel.toschi@hotmail.com>
2017-05-27 16:40:51 -0300

    gerenciador de recursos funcional

--- core/Game.java
+++ core/Game.java
@@ -6,7 +6,17 @@ package core;
  *
  */
 public class Game {
+	public static HighScores highScores = new HighScores();
+	public static ResourceManager resources = new ResourceManager();
+	
 	public static void main(String[] args) {
-
+		System.out.println(highScores);
+		
+		resources.updateCreativity(20);
+		resources.updateScalaStones(1000);
+		resources.updateScalaStones(-500);
+		resources.updateGreatRubies(-10);
+		
+		System.out.println(resources);
 	}
 }
