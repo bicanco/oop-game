125a1dd5e225cf86c81cb1f8e9b8fa0e15884107
bicanco <davidsourod@gmail.com>
2017-05-30 11:15:03 -0300

    Farm quase finalizado

--- buildings/Farm.java
+++ buildings/Farm.java
@@ -3,24 +3,28 @@ package buildings;
 public class Farm extends Building{
 	private final static int FARM_CREATIVITY_COST = 0;
 	private final static int MAX_PRODUCTION = 0;
+	private final static int SEEDS_PER_PEARL = 0;
+	private final static int SEEDS_PER_OOPYIE = 0;
+	private final static int COCOS_PER_OOPYIE = 0;
+	private final static int COCOS_PER_PYRAMID = 0;
 
 	public final static int FOOD_PRODUCTION = 0;
 	public final static int SEED_FERTILIZER = 1;
 	public final static int COCO_FERTILIZER = 2;
 	public final static int GREAT_PRODUCTION = 3;
+	public final static int NUMBER_OF_UPGRADES = 4;
+	
 	public final static boolean SEED = false;
 	public final static boolean COCO = true;
 	
  	protected int oopyiesAllocated;
 	protected boolean foodType;
-	protected static int seedsPerOopy;
-	protected static int cocosPerOopy;
 	
 	static{
 		creativityCost = FARM_CREATIVITY_COST;
-		upgradeNumber = 4;
+		upgradeNumber = NUMBER_OF_UPGRADES;
 		upgradesAvailable = new boolean[upgradeNumber];
-		upgradesAvailable[FOOD_PRODUCTION] = false;
+		upgradesAvailable[FOOD_PRODUCTION] = true;
 		upgradesAvailable[SEED_FERTILIZER] = false;
 		upgradesAvailable[COCO_FERTILIZER] = false;
 		upgradesAvailable[GREAT_PRODUCTION] = false;
@@ -40,18 +44,25 @@ public class Farm extends Building{
 	
 	public int foodProduction(){
 		if(foodType == SEED){
-			return oopyiesAllocated*seedsPerOopy;
+			return oopyiesAllocated*SEEDS_PER_OOPYIE;
 		}else if(foodType == COCO){
-			return oopyiesAllocated*cocosPerOopy;
+			return oopyiesAllocated*COCOS_PER_OOPYIE;
 		}
 		return 0;
 	}
 	
-	public static void seedFertilizer(int pearls){
+	public static int seedFertilizer(int pearls){
+		if(upgradesAvailable[SEED_FERTILIZER] == false){
+			return 0;
+		}else
+			return pearls*SEEDS_PER_PEARL;
 	}
 	
-	public static void cocoFertilizer(int pyramids){
-		
+	public static int cocoFertilizer(int pyramids){
+		if(upgradesAvailable[COCO_FERTILIZER] == false){
+			return 0;
+		}else
+			return pyramids*COCOS_PER_PYRAMID;
 	}
 	
 	public int greatProduction(int ruby){
